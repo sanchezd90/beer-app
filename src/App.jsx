0ddf0c3ad0ff393c4a4d7c89f74800c4d21bd830
Daniel <daniel.rueda@ibisdev.tech>
2023-11-25 12:55:46 -0300

    styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import html2canvas from "html2canvas";
 import { v4 as uuidv4 } from "uuid";
+import { Container, Row, Col, Form, Button, Card } from "react-bootstrap";
 
 const App = () => {
   const [formData, setFormData] = useState({
@@ -47,10 +48,8 @@ const App = () => {
       const storedCode = localStorage.getItem("generatedCode");
 
       if (storedCode) {
-        // Use the stored code if available
         setGeneratedCode(storedCode);
       } else {
-        // Generate a new code if not available
         const newCode = generateUniqueCode();
         setGeneratedCode(newCode);
         localStorage.setItem("generatedCode", newCode);
@@ -71,28 +70,28 @@ const App = () => {
   };
 
   const downloadCodeAsJPG = async () => {
-  const generatedCodeDiv = document.getElementById("generated-code-div");
+    const generatedCodeDiv = document.getElementById("generated-code-div");
 
-  // Set background color to ensure proper rendering
-  generatedCodeDiv.style.backgroundColor = '#fff'; // Set to your desired background color
+    // Set background color to ensure proper rendering
+    generatedCodeDiv.style.backgroundColor = "#F5F5DC";
 
-  // Wait for a short delay to ensure elements are fully rendered
-  await new Promise(resolve => setTimeout(resolve, 500));
+    // Wait for a short delay to ensure elements are fully rendered
+    await new Promise((resolve) => setTimeout(resolve, 500));
 
-  html2canvas(generatedCodeDiv).then((canvas) => {
-    // Reset background color to its original state
-    generatedCodeDiv.style.backgroundColor = '';
+    html2canvas(generatedCodeDiv).then((canvas) => {
+      // Reset background color to its original state
+      generatedCodeDiv.style.backgroundColor = "";
 
-    // Convert canvas to JPEG image
-    const imageData = canvas.toDataURL("image/jpeg");
+      // Convert canvas to JPEG image
+      const imageData = canvas.toDataURL("image/jpeg");
 
-    // Create a link and trigger the download
-    const link = document.createElement("a");
-    link.href = imageData;
-    link.download = "generated_code.jpg";
-    link.click();
-  });
-};
+      // Create a link and trigger the download
+      const link = document.createElement("a");
+      link.href = imageData;
+      link.download = "generated_code.jpg";
+      link.click();
+    });
+  };
 
   useEffect(() => {
     const storedUserData = localStorage.getItem("userData");
@@ -104,99 +103,144 @@ const App = () => {
       twentyFourHoursAgo.setHours(twentyFourHoursAgo.getHours() - 24);
 
       if (lastSubmissionTime > twentyFourHoursAgo) {
-        // Show last generated code
         setGeneratedCode(localStorage.getItem("generatedCode"));
         setFormSubmitted(true);
       } else {
-        // Clear local storage
         localStorage.removeItem("userData");
         localStorage.removeItem("formSubmissionTime");
-        localStorage.removeItem("generatedCode"); // Clear generated code when refreshing
+        localStorage.removeItem("generatedCode");
       }
     }
   }, []);
 
   return (
-    <div className="App">
-      <h1>Code Generator</h1>
-      {!formSubmitted ? (
-        <form>
-          <div>
-            <label>Name:</label>
-            <input
-              type="text"
-              value={formData.name}
-              onChange={(e) =>
-                setFormData({ ...formData, name: e.target.value })
-              }
-            />
-          </div>
-          <div>
-            <label>ID:</label>
-            <input
-              type="text"
-              value={formData.id}
-              onChange={(e) => setFormData({ ...formData, id: e.target.value })}
-            />
-          </div>
-          <div>
-            <label>Date of Birth:</label>
-            <input
-              type="date"
-              value={formData.dob}
-              onChange={(e) =>
-                setFormData({ ...formData, dob: e.target.value })
-              }
-            />
-          </div>
-          <div>
-            <label>Instagram:</label>
-            <input
-              type="text"
-              value={formData.instagram}
-              onChange={(e) =>
-                setFormData({ ...formData, instagram: e.target.value })
-              }
-            />
-          </div>
-          {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
-          <button type="button" onClick={generateCode}>
-            Generate Code
-          </button>
-        </form>
-      ) : (
-        <div>
-          <div id="generated-code-div">
-            <h2>User Data</h2>
-            <p>
-              Name:{" "}
-              {localStorage.getItem("userData") &&
-                JSON.parse(localStorage.getItem("userData")).name}
-            </p>
-            <p>
-              ID:{" "}
-              {localStorage.getItem("userData") &&
-                JSON.parse(localStorage.getItem("userData")).id}
-            </p>
-            <p>
-              Date of Birth:{" "}
-              {localStorage.getItem("userData") &&
-                JSON.parse(localStorage.getItem("userData")).dob}
-            </p>
-            <p>
-              Instagram:{" "}
-              {localStorage.getItem("userData") &&
-                JSON.parse(localStorage.getItem("userData")).instagram}
-            </p>
-            <h2>Generated Code</h2>
-            <p>{generatedCode}</p>
-          </div>
-          <button type="button" onClick={downloadCodeAsJPG}>
-            Download Code as JPG
-          </button>
-        </div>
-      )}
-    </div>
+    <Container className="App" id="generated-code-div">
+      <h1 style={{ color: "#FF6F61", backgroundColor:'#F5F5DC', margin:'0px', padding: "20px 20px", borderTopLeftRadius:'10px',borderTopRightRadius:'10px' }}>Beer Coupon</h1>
+      <div className="main-container">
+        {!formSubmitted ? (
+          <Form className="my-form">
+            <Row className="mb-3">
+              <Col>
+                <Form.Group controlId="formName">
+                  <Form.Label style={{ color: "#34495e" }}>Name</Form.Label>
+                  <Form.Control
+                    type="text"
+                    value={formData.name}
+                    onChange={(e) =>
+                      setFormData({ ...formData, name: e.target.value })
+                    }
+                    className="my-input"
+                  />
+                </Form.Group>
+              </Col>
+              <Col>
+                <Form.Group controlId="formID">
+                  <Form.Label style={{ color: "#34495e" }}>ID</Form.Label>
+                  <Form.Control
+                    type="text"
+                    value={formData.id}
+                    onChange={(e) =>
+                      setFormData({ ...formData, id: e.target.value })
+                    }
+                    className="my-input"
+                  />
+                </Form.Group>
+              </Col>
+            </Row>
+            <Row className="mb-3">
+              <Col>
+                <Form.Group controlId="formDob">
+                  <Form.Label style={{ color: "#34495e" }}>
+                    Date of Birth
+                  </Form.Label>
+                  <div className="date-picker-container">
+                    <Form.Control
+                      type="date"
+                      value={formData.dob}
+                      onChange={(e) =>
+                        setFormData({ ...formData, dob: e.target.value })
+                      }
+                      className="my-input"
+                    />                    
+                  </div>
+                </Form.Group>
+              </Col>
+              <Col>
+                <Form.Group controlId="formInstagram">
+                  <Form.Label style={{ color: "#34495e" }}>
+                    Instagram
+                  </Form.Label>
+                  <Form.Control
+                    type="text"
+                    value={formData.instagram}
+                    onChange={(e) =>
+                      setFormData({ ...formData, instagram: e.target.value })
+                    }
+                    className="my-input"
+                  />
+                </Form.Group>
+              </Col>
+            </Row>
+            {errorMessage && (
+              <p className="error-message" style={{ color: "#e74c3c" }}>
+                {errorMessage}
+              </p>
+            )}
+            <Button
+              className="my-button"
+              variant="primary"
+              type="button"
+              onClick={generateCode}
+              style={{
+                backgroundColor: "#FF6F61",
+                border: "none",
+                marginBottom: "20px",
+              }}
+            >
+              Generate Code
+            </Button>
+          </Form>
+        ) : (
+          <Card>
+            <Card.Body>
+              <div>
+                <h2 style={{ color: "#FF6F61" }}>User Data</h2>
+                <p>
+                  <strong>Name:</strong>{" "}
+                  {localStorage.getItem("userData") &&
+                    JSON.parse(localStorage.getItem("userData")).name}
+                </p>
+                <p>
+                  <strong>ID:</strong>{" "}
+                  {localStorage.getItem("userData") &&
+                    JSON.parse(localStorage.getItem("userData")).id}
+                </p>
+                <p>
+                  <strong>Date of Birth:</strong>{" "}
+                  {localStorage.getItem("userData") &&
+                    JSON.parse(localStorage.getItem("userData")).dob}
+                </p>
+                <p>
+                  <strong>Instagram:</strong>{" "}
+                  {localStorage.getItem("userData") &&
+                    JSON.parse(localStorage.getItem("userData")).instagram}
+                </p>
+                <h2 style={{ color: "#FF6F61" }}>Generated Code</h2>
+                <p>{generatedCode}</p>
+              </div>
+              <Button
+                variant="success"
+                type="button"
+                onClick={downloadCodeAsJPG}
+                style={{ backgroundColor: "#FF6F61", border: "none" }}
+              >
+                Download Code as JPG
+              </Button>
+            </Card.Body>
+          </Card>
+        )}
+      </div>
+    </Container>
   );
 };
 
